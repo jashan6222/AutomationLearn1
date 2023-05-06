fb2fb199c6db4a6d4bdf4ce44991909a1df80267
jashan6222 <jashan6222@gmail.com>
2023-05-07 00:42:29 +0530

    FunctionsConcept

--- src/test/java/OOPSConceptsPart1/FunctionsConcept.java
+++ src/test/java/OOPSConceptsPart1/FunctionsConcept.java
@@ -3,5 +3,37 @@ package OOPSConceptsPart1;
 public class FunctionsConcept {
     public static void main(String[] args) {
 
+        FunctionsConcept functionsConcept=new FunctionsConcept();
+        functionsConcept.test();
+        System.out.println(functionsConcept.qa
+                (functionsConcept.divisionMethod
+                        (functionsConcept.methodTwo(),functionsConcept.justReturn10())));
+
+
+    }
+    public void test(){
+        System.out.println("Test Method");
+    }
+
+    public int methodTwo(){
+        System.out.println("methodTwo Method");
+
+        int a=10;
+        int b=20;
+        int c=a+b;
+        return c;
+    }
+
+    public String qa(double x){
+        String s="Selenium";
+        return s+" "+x;
+    }
+
+    public int justReturn10(){
+        return 10;
+    }
+    public double divisionMethod(int x, int y){
+        double d=x/y;
+        return d;
     }
 }
