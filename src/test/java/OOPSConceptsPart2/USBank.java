c2320c84582290ae72e211b0d2d8abbcb4fdbcd5
jashan6222 <jashan6222@gmail.com>
2023-05-09 21:55:41 +0530

    Interface

---
+++ src/test/java/OOPSConceptsPart2/USBank.java
@@ -0,0 +1,18 @@
+package OOPSConceptsPart2;
+
+public interface USBank {
+
+    int min_Balance = 100;
+
+    public void credit();
+    public void debit();
+    public void transferMoney();
+
+    // PROPERTIES OF INTERFACE
+    //1. Only Method declaration
+    //2. we can declare the variables, which are by default "static"
+    //3. Variable value cannot be changed
+    //4. No static method in interface
+    //5. No main method in interface
+    //6. We cannot create object of interface --> Abstract in nature
+}
